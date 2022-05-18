57d08f3ef2abe78f843d4ac94f56a873a85d3a44
Abed Khalaf <kfabdall@gmail.com>
2022-05-18 23:35:21 +0300

    new exercise

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import SeasonDisplay from './SeasonDisplay'
+import { createRoot } from 'react-dom/client';
+
+class App extends React.Component {
+ state = { lat: null, errMessage: ''}
+
+  componentDidMount () {
+   window.navigator.geolocation.getCurrentPosition(
+   position => this.setState({lat: position.coords.latitude}),
+   err => this.setState({ errMessage : err.message})
+)}
+
+  render() {
+     if (this.state.errMessage && !this.state.lat){
+        return <div>Error: {this.state.errMessage}</div>
+     
+     }
+     if (!this.state.errMessage && this.state.lat){
+      return <SeasonDisplay lat={this.state.lat} />
+     }
+ return <div>Loading!</div>
+}
+}
+const container = document.getElementById('root')
+const root = createRoot(container);
+root.render(<App />);
+
+
+
